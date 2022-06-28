b6ef3b3e3b7744911e2f2bff536fa3bcd9e7f940
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-06-28 19:18:54 +1000

    fix html error

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,49 +21,48 @@ var recipeHistoryEl = document.getElementById('recipe-history');
 //Event listener that runs search function on click
 
 var newSearch = document.getElementById('searchBtn');
-newSearch.addEventListener("click",fn1);
+newSearch.addEventListener("click", fn1);
 
 // Fetch list of recipe names from Spoonacular API based on ingredient inputs
 
-function fn1(e)
-{
+function fn1(e) {
 
 
-   var ingred = document.getElementById('form1').value;
-   var ingred2 = document.getElementById('form2').value;
-   var ingred3 = document.getElementById('form3').value;
-   var allIngreds = ingred + ",+" + ingred2 + ",+" + ingred3;
+    var ingred = document.getElementById('form1').value;
+    var ingred2 = document.getElementById('form2').value;
+    var ingred3 = document.getElementById('form3').value;
+    var allIngreds = ingred + ",+" + ingred2 + ",+" + ingred3;
 
-   console.log (allIngreds);
+    console.log(allIngreds);
 
-   var newRecipe = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='+ allIngreds + '&number=10&apiKey=39791063581a4d96a908bb19745b3f64';
- 
- 
-   fetch(newRecipe)
-      .then(response => {
-         if (!response.ok){
-            throw Error("ERROR")
-         };
-         return response.json();
-   })
-   .then(data => {
-      console.log(data);
-
-      document.querySelector('#result1').innerHTML;
-   }).catch(error =>{
-      console.log(error)
-   });
+    var newRecipe = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + allIngreds + '&number=10&apiKey=39791063581a4d96a908bb19745b3f64';
 
 
+    fetch(newRecipe)
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR")
+            };
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
 
-   var inputs = document.querySelectorAll('#form1, #form2, #form3')
-   inputs.forEach(input => {
-      input.value = '';
-   });
+            document.querySelector('#result1').innerHTML;
+        }).catch(error => {
+            console.log(error)
+        });
 
-   e.preventDefault();
 
- 
+
+    var inputs = document.querySelectorAll('#form1, #form2, #form3')
+    inputs.forEach(input => {
+        input.value = '';
+    });
+
+    e.preventDefault();
+
+
 };
 
 <<<<<<< HEAD
@@ -82,7 +81,7 @@ fetch(searchResults).then(function (response) {
     console.log(data);
     // checks if videoId is undefined, in the case that the first result is not a video
     for (var i = 0; i <= data.items.length; i++) {
-        if(data.items[i].id.videoId === undefined) {
+        if (data.items[i].id.videoId === undefined) {
             continue;
         } else {
             videoID = data.items[i].id.videoId;
@@ -98,32 +97,32 @@ fetch(searchResults).then(function (response) {
 // check if below functions are needed...
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: videoID,
-    playerVars: {
-        'playsinline': 1
+        height: '390',
+        width: '640',
+        videoId: videoID,
+        playerVars: {
+            'playsinline': 1
         },
         events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
         }
-        });
+    });
 }
 
 function onPlayerReady(event) {
-event.target.playVideo();
+    event.target.playVideo();
 }
 
 var done = false;
 function onPlayerStateChange(event) {
-if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-}
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
 }
 function stopVideo() {
-player.stopVideo();
+    player.stopVideo();
 }
 =======
 
@@ -148,36 +147,36 @@ player.stopVideo();
 
 // Save recipe in local storage
 function saveRecipe() {
-   var savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
+    var savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
 
-   if (savedRecipes === null) {
-      savedRecipes = [selectedRecipe];
-   } else {
-      savedRecipes.push(selectedRecipe);
-   }
+    if (savedRecipes === null) {
+        savedRecipes = [selectedRecipe];
+    } else {
+        savedRecipes.push(selectedRecipe);
+    }
 
-   localStorage.setItem("savedRecipes", JSON.stringify(savedRecipes));
-   showRecipeHistory();
+    localStorage.setItem("savedRecipes", JSON.stringify(savedRecipes));
+    showRecipeHistory();
 }
 
 // Display recipe in search history
 function showRecipeHistory() {
-   recipeHistoryEl.innerHTML = '';
-    
-   var savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
-   
-   if (savedRecipes !== null) {
-       for (var i = 0; i < savedRecipes.length; i++) {
-           var recipe = savedRecipes[i];
-           var li = document.createElement("li");
-           li.classList = 'btn recipe-history__list-group-item';
-           li.textContent = recipe;
-           recipeHistoryEl.appendChild(li);
-       }
-   }
+    recipeHistoryEl.innerHTML = '';
+
+    var savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
+
+    if (savedRecipes !== null) {
+        for (var i = 0; i < savedRecipes.length; i++) {
+            var recipe = savedRecipes[i];
+            var li = document.createElement("li");
+            li.classList = 'btn recipe-history__list-group-item';
+            li.textContent = recipe;
+            recipeHistoryEl.appendChild(li);
+        }
+    }
 }
 
 // When page loads, load search history (unsure if we want anything else here?)
-window.onload = function() {
-   showRecipeHistory();
+window.onload = function () {
+    showRecipeHistory();
 }
